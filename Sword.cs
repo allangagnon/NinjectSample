71cefee3e4e088e06d5c895b86ce6b5afa879ce0
Allan F. Gagnon <allan.gagnon@gmail.com>
2013-03-29 13:09:25 -0500

    C# Class Sword that implements IWeapon
    C# Class Sword that implements IWeapon

---
+++ Sword.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace SamuraiNinject
+{
+    internal class Sword : IWeapon
+    {
+        public void Hit(string target)
+        {
+            Console.WriteLine("Chopped {0} clean in half", target);
+        }
+    }
+}
